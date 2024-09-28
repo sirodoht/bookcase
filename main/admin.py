15097f291ed2544f895d38b0eec7971a4e0cdc19
Theodore Keloglou <zf@sirodoht.com>
2024-09-28 12:10:59 +0100

    Add models and templates

--- main/admin.py
+++ main/admin.py
@@ -1,2 +1,22 @@
+from django.contrib import admin
 
-# Register your models here.
+from main import models
+
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "published_at",
+    )
+    ordering = ["-id"]
+
+
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "full_name",
+    )
+    ordering = ["-id"]
--- main/models.py
+++ main/models.py
@@ -1,2 +1,27 @@
+from datetime import datetime
 
-# Create your models here.
+from django.core import validators
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Book(models.Model):
+    title = models.CharField(_("book title"), max_length=200, unique=True)
+    published_at = models.PositiveIntegerField(
+        _("year of publication"),
+        default=datetime.now().year,
+        validators=[
+            validators.MinValueValidator(1455),
+            validators.MaxValueValidator(2540),
+        ],
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    full_name = models.CharField(_("book title"), max_length=200)
+
+    def __str__(self):
+        return self.full_name
