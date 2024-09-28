ee58597825930918dce460b41c912b76d3c6a422
Theodore Keloglou <zf@sirodoht.com>
2024-09-28 15:26:28 +0100

    Add book list on index template

--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from main import models
+
 
 def index(request):
-    return render(request, "main/index.html")
+    return render(
+        request,
+        "main/index.html",
+        {"book_list": models.Book.objects.all()},
+    )
