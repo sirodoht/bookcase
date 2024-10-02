7d029f8b016d88792f34f7e1d7eb267ab07ca484
Theodore Keloglou <zf@sirodoht.com>
2024-10-02 22:38:38 +0100

    Add django media settings

--- bookcase/settings.py
+++ bookcase/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
 
+MEDIA_URL = "files/"
+MEDIA_ROOT = BASE_DIR / "files"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
